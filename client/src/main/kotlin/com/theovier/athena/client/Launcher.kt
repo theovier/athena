0460d7fbb7ea73ef9c76bd782de5a520350dc87b
theovier <Theo@Harkenbusch.com>
2021-07-24 16:55:37 +0200

    add title to game and mv mvn compiler source into client

--- client/src/main/kotlin/com/theovier/athena/client/Launcher.kt
+++ client/src/main/kotlin/com/theovier/athena/client/Launcher.kt
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 
 fun main() {
     val config = Lwjgl3ApplicationConfiguration().apply {
+        setTitle("Athena")
         setWindowSizeLimits(1920, 1200, 1920, 1200)
     }
     Lwjgl3Application(AthenaGame(), config)

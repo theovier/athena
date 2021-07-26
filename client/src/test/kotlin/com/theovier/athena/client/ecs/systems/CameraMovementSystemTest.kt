9a7cd2a63e659505264e067b9685163bab355806
theovier <Theo@Harkenbusch.com>
2021-07-26 21:09:32 +0200

    add camera movement system tests

---
+++ client/src/test/kotlin/com/theovier/athena/client/ecs/systems/CameraMovementSystemTest.kt
@@ -0,0 +1,72 @@
+package com.theovier.athena.client.ecs.systems
+
+import com.badlogic.ashley.core.Engine
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.utils.GdxNativesLoader
+import com.theovier.athena.client.ecs.components.CameraTarget
+import com.theovier.athena.client.ecs.components.Movement
+import com.theovier.athena.client.ecs.components.Transform
+import io.mockk.impl.annotations.MockK
+import ktx.ashley.entity
+import ktx.ashley.get
+import ktx.ashley.with
+import org.junit.jupiter.api.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CameraMovementSystemTest {
+
+    private var camera = OrthographicCamera()
+
+    companion object {
+        const val DELTA_TIME = 0.1f
+        val STARTING_POSITION = Vector3(1f, 1f, 0f)
+    }
+
+    @BeforeAll
+    fun setup() {
+        GdxNativesLoader.load();
+    }
+
+    @BeforeEach
+    fun reset() {
+        camera = OrthographicCamera()
+        camera.position.set(STARTING_POSITION)
+    }
+
+    @Test
+    @DisplayName("Camera follows entity with <CameraTarget> component")
+    fun cameraFollowsTarget() {
+        val engine = Engine().apply {
+            addSystem(CameraMovementSystem(camera))
+        }
+        val entity = engine.entity {
+            with<CameraTarget>()
+            with<Transform>()
+            with<Movement>() {
+                maxSpeed = 10f
+                accelerationFactor = 10f
+                direction = Vector2.X
+            }
+        }
+        engine.update(DELTA_TIME)
+        Assertions.assertTrue(camera.position != STARTING_POSITION)
+    }
+
+    @Test
+    @DisplayName("Camera does not move when entity with <CameraTarget> component is standing still")
+    fun cameraDoesNotMoveWhenTargetIsStandingStill() {
+        val engine = Engine().apply {
+            addSystem(CameraMovementSystem(camera))
+        }
+        val entity = engine.entity {
+            with<CameraTarget>()
+            with<Transform>() {
+                position.set(STARTING_POSITION)
+            }
+        }
+        engine.update(DELTA_TIME)
+        Assertions.assertTrue(camera.position == STARTING_POSITION)
+    }
+}

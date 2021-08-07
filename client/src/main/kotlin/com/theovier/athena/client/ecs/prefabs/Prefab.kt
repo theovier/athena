27de135b2f261e6d0ada07fd0e0173a28373634e
theovier <Theo@Harkenbusch.com>
2021-08-08 00:11:45 +0200

    add simple example how to components could be added

--- client/src/main/kotlin/com/theovier/athena/client/ecs/prefabs/Prefab.kt
+++ client/src/main/kotlin/com/theovier/athena/client/ecs/prefabs/Prefab.kt
@@ -1,36 +1,55 @@
 package com.theovier.athena.client.ecs.prefabs
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.Engine
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.core.PooledEngine
+import com.theovier.athena.client.ecs.components.Transform
 import kotlinx.serialization.Polymorphic
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import ktx.ashley.addComponent
+import ktx.ashley.get
+import ktx.ashley.mapperFor
+import ktx.ashley.plusAssign
 import mu.KotlinLogging
 import nl.adaptivity.xmlutil.serialization.XmlPolyChildren
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
+import java.lang.RuntimeException
 
 @Serializable
 data class Prefab(@XmlPolyChildren(arrayOf(".Foo", ".Bar")) val components: List<@Polymorphic Component>)
 
 @Serializable
-class Foo(val name: String) : Component
+class Foo(var name: String) : Component
 
 @Serializable
-class Bar(val id: Int) : Component
+class Bar(var id: Int) : Component
+
 
 
 private val log = KotlinLogging.logger {}
 fun main() {
-    val prefab = GameObject.instantiate("player")
+    val engine = PooledEngine()
+    val entity = Entity()
 
+    val prefab = GameObject.instantiate("player")
     prefab.components.forEach {
-        when(it) {
+        val component = when(it) {
             is Foo -> {
-                log.info { it.name }
+                Foo(it.name)
             }
             is Bar -> {
-                log.info { it.id }
+                Bar(it.id)
+            }
+            else -> {
+                log.error { "A component in this prefab was not recognized and therefore not added." }
+                return
             }
         }
+        entity += component
     }
+    engine.addEntity(entity)
 
+    log.info { entity.get<Foo>()?.name }
 }

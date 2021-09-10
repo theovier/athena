1e5d644948431ad28e8c3bfca33489d93bca9581
theovier <Theo@Harkenbusch.com>
2021-09-10 20:14:50 +0200

    change return type of tiled map component loader

--- client/src/main/kotlin/com/theovier/athena/client/ecs/prefabs/loaders/components/TiledMapComponentLoader.kt
+++ client/src/main/kotlin/com/theovier/athena/client/ecs/prefabs/loaders/components/TiledMapComponentLoader.kt
@@ -1,12 +1,11 @@
 package com.theovier.athena.client.ecs.prefabs.loaders.components
 
-import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.utils.JsonValue
 import com.theovier.athena.client.ecs.components.TiledMap
 
 class TiledMapComponentLoader : ComponentLoader {
-    override fun load(componentJSON: JsonValue): Component {
+    override fun load(componentJSON: JsonValue): TiledMap {
         val component = TiledMap()
         val file = componentJSON.asString()
         component.map = TmxMapLoader().load(file)

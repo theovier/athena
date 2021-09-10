859d267682d97f5d25792f4d723de43c9297fb75
theovier <Theo@Harkenbusch.com>
2021-09-10 12:48:30 +0200

    prepare tests

---
+++ client/src/test/kotlin/com/theovier/athena/client/ecs/prefabs/loaders/components/TransformComponentLoaderTest.kt
@@ -0,0 +1,43 @@
+package com.theovier.athena.client.ecs.prefabs.loaders.components
+
+import org.junit.Test
+import org.junit.jupiter.api.DisplayName
+
+class TransformComponentLoaderTest {
+
+    @Test
+    @DisplayName("<Transform> component is loaded correctly")
+    fun isTransformComponentLoadedCorrectly() {
+
+    }
+
+    @Test
+    @DisplayName("Empty <Transform> component is loaded correctly")
+    fun isEmptyTransformComponentLoadedCorrectly() {
+
+    }
+
+    @Test
+    @DisplayName("<Transform> with <size> is loaded correctly")
+    fun isSizeLoadedCorrectly() {
+
+    }
+
+    @Test
+    @DisplayName("<Transform> with <position> is loaded correctly")
+    fun isPositionLoadedCorrectly() {
+
+    }
+
+    @Test
+    @DisplayName("<Transform> with both <position> and <size> is loaded correctly")
+    fun isPositionAndSizeLoadedCorrectly() {
+
+    }
+
+    @Test
+    @DisplayName("Unrecognizable elements are ignored")
+    fun areUnrecognizableElementsIgnored() {
+
+    }
+}

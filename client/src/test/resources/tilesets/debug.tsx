008841a2e3115a01723deb4749ab09eac45ded42
theovier <Theo@Harkenbusch.com>
2021-09-10 20:14:59 +0200

    add prerequists to test map loader

---
+++ client/src/test/resources/tilesets/debug.tsx
@@ -0,0 +1,9 @@
+<tileset version="1.5" tiledversion="1.7.2" name="debug" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../sprites/gray_dark_square.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../sprites/gray_light_square.png"/>
+ </tile>
+</tileset>

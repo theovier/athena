b0c95be4d82f280ff7a3f0bf0a603d0470d9ae96
theovier <Theo@Harkenbusch.com>
2021-09-14 15:22:05 +0200

    import damage as alias because of conflicting namespace

--- client/src/main/kotlin/com/theovier/athena/client/ecs/components/Damage.kt
+++ client/src/main/kotlin/com/theovier/athena/client/ecs/components/Damage.kt
@@ -3,15 +3,15 @@ package com.theovier.athena.client.ecs.components
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Entity
 import com.badlogic.gdx.utils.GdxRuntimeException
-import com.theovier.athena.client.weapons.Damage
+import com.theovier.athena.client.weapons.Damage as Dmg
 import ktx.ashley.get
 import ktx.ashley.mapperFor
 
-class Damage(val damage: Damage) : Component {
+class Damage(val damage: Dmg) : Component {
     companion object {
-        val MAPPER = mapperFor<com.theovier.athena.client.ecs.components.Damage>()
+        val MAPPER = mapperFor<Damage>()
     }
 }
 
-val Entity.damageComponent: com.theovier.athena.client.ecs.components.Damage
-    get() = this[com.theovier.athena.client.ecs.components.Damage.MAPPER] ?: throw GdxRuntimeException("Damage for entity '$this' is null")
+val Entity.damageComponent: Damage
+    get() = this[Damage.MAPPER] ?: throw GdxRuntimeException("Damage for entity '$this' is null")

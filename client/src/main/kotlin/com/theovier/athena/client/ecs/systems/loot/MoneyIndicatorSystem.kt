2f35d514baf94d9fd4c1e7dc89c0683b8d107918
theovier <theo@harkenbusch.com>
2022-02-17 23:20:29 +0100

    add possibility of negative money loot

--- client/src/main/kotlin/com/theovier/athena/client/ecs/systems/loot/MoneyIndicatorSystem.kt
+++ client/src/main/kotlin/com/theovier/athena/client/ecs/systems/loot/MoneyIndicatorSystem.kt
@@ -7,20 +7,25 @@ import com.theovier.athena.client.ecs.components.*
 import com.theovier.athena.client.ecs.components.movement.Velocity
 import com.theovier.athena.client.ecs.prefabs.Prefab
 import ktx.ashley.allOf
+import kotlin.math.absoluteValue
 
 class MoneyIndicatorSystem : IteratingSystem(allOf(Looted::class, Money::class).get()) {
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
+        val money = entity.money
         val looter = entity.looted.lootedBy
         val position = looter.transform.position
+
+        val signString = if(money.amount >= 0) "+" else "-"
+        val amountString = "$ ${money.amount.absoluteValue}"
 
         Prefab.instantiate("moneyGainIndicator").apply {
             with(transform) {
                 this.position.set(position.x, this.position.y + position.y, position.z)
             }
             with(text) {
-                text = "+ $ ${entity.money.amount}"
-                color = Color.GREEN //todo add color to text component loader
+                text = "$signString $amountString"
+                color = if(money.amount >= 0) Color.GREEN else Color.RED
             }
             //todo add fade system for text
         }

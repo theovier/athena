eda1afdaa8c2b0934833f461c22c68445ab9a247
theovier <theo@harkenbusch.com>
2022-03-24 19:21:18 +0100

    add simple dot system

---
+++ client/src/main/kotlin/com/theovier/athena/client/ecs/systems/damage/DamageOverTimeSystem.kt
@@ -0,0 +1,35 @@
+package com.theovier.athena.client.ecs.systems.damage
+
+import com.badlogic.ashley.core.Entity
+import com.badlogic.ashley.systems.IteratingSystem
+import com.theovier.athena.client.ecs.components.*
+import com.theovier.athena.client.weapons.Damage
+import ktx.ashley.allOf
+import ktx.ashley.exclude
+
+class DamageOverTimeSystem : IteratingSystem(allOf(DamageOverTime::class).exclude(Invincible::class).get()) {
+    override fun processEntity(victim: Entity, deltaTime: Float) {
+        val dot = victim.dot
+
+        if (dot.isExpired) {
+            victim.remove(DamageOverTime::class.java)
+            return
+        }
+
+        dot.duration -= deltaTime
+        dot.nextTick -= deltaTime
+
+        if (dot.shouldTick) {
+            tick(victim, dot.damage)
+            dot.nextTick = dot.tickRate
+        }
+    }
+
+    private fun tick(victim: Entity, damage: Damage) {
+        if (victim.hasNoHitMarkerComponent) {
+            victim.add(HitMarker())
+        }
+        val hitmarker = victim.hitmarker
+        hitmarker.onHit(damage)
+    }
+}
